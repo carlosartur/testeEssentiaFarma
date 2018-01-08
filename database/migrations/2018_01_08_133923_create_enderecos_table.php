102254ddb7bc5697560eb8ef338afe8f8b756867
suporte <suporte@essentia.local>
2018-01-08 11:52:19 -0200

    Migration de endereços

---
+++ database/migrations/2018_01_08_133923_create_enderecos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEnderecosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+			$table->engine = 'InnoDB';
+            $table->increments('id');
+			$table->string('rua', 255);
+			$table->string('numero', 255);
+			$table->string('cidade', 255);
+			$table->string('estado', 255);
+			$table->string('pais', 255);
+			$table->integer('cliente_id')->unsigned();
+			$table->foreign('cliente_id')->references('id')->on('clientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enderecos');
+    }
+}
