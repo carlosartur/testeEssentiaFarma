797c391c7033933b48363c20b38608bf1bb7f7fc
suporte <suporte@essentia.local>
2018-01-08 11:49:32 -0200

    Listagem inicial

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Cliente;
 use Illuminate\Http\Request;
 
 class ClientesController extends Controller
 {
-    //
+    public function listar()
+	{
+		$Clientes = Cliente::get();
+		return view('clientes.listar')->with(compact('Clientes'));
+	}
 }
